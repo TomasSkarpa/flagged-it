6d19cc9260559d9502cb4d28b60a42470608434c
Tomáš Škarpa <tskarpam8@gmail.com>
2025-11-27 00:36:09 +0100

    feat: graphic ui for the guessing parameter game

--- internal/games/guessing/game.go
+++ internal/games/guessing/game.go
@@ -1,39 +1,62 @@
 package guessing
 
 import (
-	"strings"
 	"fmt"
+	"image/color"
 	"math/rand"
+	"runtime"
+	"strings"
 
 	"flagged-it/internal/data"
 	"flagged-it/internal/data/models"
 	"flagged-it/internal/ui/components"
+
 	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/widget"
 	"fyne.io/fyne/v2/theme"
+	"fyne.io/fyne/v2/widget"
 )
+
+type fixedHeightTile struct {
+	widget.BaseWidget
+	bg      *canvas.Rectangle
+	content fyne.CanvasObject
+	height  float32
+}
+
+func newFixedHeightTile(bg *canvas.Rectangle, content fyne.CanvasObject, height float32) *fixedHeightTile {
+	t := &fixedHeightTile{bg: bg, content: content, height: height}
+	t.ExtendBaseWidget(t)
+	return t
+}
+
+func (t *fixedHeightTile) CreateRenderer() fyne.WidgetRenderer {
+	return widget.NewSimpleRenderer(container.NewStack(t.bg, t.content))
+}
+
+func (t *fixedHeightTile) MinSize() fyne.Size {
+	return fyne.NewSize(0, t.height)
+}
 
 type Game struct {
 	content        *fyne.Container
-	popContent     *fyne.Container
-	areaContent    *fyne.Container
 	backFunc       func()
 	countries      []models.Country
 	currentCountry *models.Country
 	guessEntry     *widget.Entry
 	statusLabel    *widget.Label
-	popLabel	   *widget.Label
-	areaLabel	   *widget.Label
-	continentLabel *widget.Label
-	popIcon        *widget.Icon
-	areaIcon	   *widget.Icon
 	guessBtn       *widget.Button
+	headerGrid     *fyne.Container
+	bodyGrid       *fyne.Container
+	bodyScroll     *container.Scroll
+	guesses        []models.Country
 }
 
 func NewGame(backFunc func()) *Game {
 	g := &Game{
 		backFunc: backFunc,
+		guesses:  []models.Country{},
 	}
 	g.loadCountries()
 	g.setupUI()
@@ -48,15 +71,7 @@ func (g *Game) loadCountries() {
 func (g *Game) setupUI() {
 	topBar := components.NewTopBar("What country is this?", g.backFunc, g.newGame)
 
-	g.continentLabel = widget.NewLabel("Continent: ")
-	g.popLabel = widget.NewLabel("Population: ")
-	g.areaLabel = widget.NewLabel("Area: ")
 	g.statusLabel = widget.NewLabel("Make a guess!")
-
-	g.popIcon = widget.NewIcon(theme.MoveUpIcon())
-	g.areaIcon = widget.NewIcon(theme.MoveUpIcon())
-	g.popIcon.Hide()
-	g.areaIcon.Hide()
 
 	g.guessEntry = widget.NewEntry()
 	g.guessEntry.SetPlaceHolder("Enter country name...")
@@ -65,58 +80,148 @@ func (g *Game) setupUI() {
 	g.guessBtn = widget.NewButton("Guess", g.makeGuess)
 
 	guessContainer := container.NewGridWithColumns(2, g.guessEntry, g.guessBtn)
-	g.popContent = container.NewHBox(g.popLabel, g.popIcon)
-	g.areaContent = container.NewHBox(g.areaLabel, g.areaIcon)
 
-	g.content = container.NewVBox(
-		topBar.GetContainer(),
+	g.headerGrid = container.NewGridWithColumns(5)
+	g.addHeaderRow()
+
+	g.bodyGrid = container.NewVBox()
+	g.bodyScroll = container.NewVScroll(g.bodyGrid)
+
+	topSection := container.NewVBox(
 		widget.NewSeparator(),
 		g.statusLabel,
 		widget.NewSeparator(),
-		widget.NewSeparator(),
 		guessContainer,
-		g.continentLabel,
-		g.popContent,
-		g.areaContent,
+		widget.NewSeparator(),
+		widget.NewLabel("Guess History:"),
+		g.headerGrid,
+	)
+
+	g.content = container.NewBorder(
+		topBar.GetContainer(),
+		nil,
+		nil,
+		nil,
+		container.NewBorder(
+			topSection,
+			nil,
+			nil,
+			nil,
+			g.bodyScroll,
+		),
 	)
 }
 
+func (g *Game) addHeaderRow() {
+	g.headerGrid.Add(g.createTile("Flag", nil, color.RGBA{100, 100, 100, 255}))
+	g.headerGrid.Add(g.createTile("Country", nil, color.RGBA{100, 100, 100, 255}))
+	g.headerGrid.Add(g.createTile("Continent", nil, color.RGBA{100, 100, 100, 255}))
+	g.headerGrid.Add(g.createTile("Population", nil, color.RGBA{100, 100, 100, 255}))
+	g.headerGrid.Add(g.createTile("Area", nil, color.RGBA{100, 100, 100, 255}))
+}
+
+func (g *Game) createTile(text string, icon fyne.Resource, bgColor color.Color) fyne.CanvasObject {
+	bg := canvas.NewRectangle(bgColor)
+	label := canvas.NewText(text, color.White)
+	label.TextSize = 24
+	label.TextStyle = fyne.TextStyle{Bold: true}
+	label.Alignment = fyne.TextAlignCenter
+
+	var content fyne.CanvasObject
+	if icon != nil {
+		iconWidget := widget.NewIcon(icon)
+		content = container.NewHBox(label, iconWidget)
+	} else {
+		content = container.NewVBox(label)
+	}
+
+	return newFixedHeightTile(bg, container.NewCenter(content), 50)
+}
+
+func (g *Game) getCompareIcon(guessVal, targetVal float64) fyne.Resource {
+	if guessVal < targetVal {
+		return theme.MoveUpIcon()
+	} else if guessVal > targetVal {
+		return theme.MoveDownIcon()
+	}
+	return nil
+}
+
+func (g *Game) getCompareColor(guessVal, targetVal float64) color.Color {
+	if guessVal == targetVal {
+		return color.RGBA{0, 180, 0, 255}
+	}
+	return color.RGBA{180, 0, 0, 255}
+}
+
+func (g *Game) createFlagTile(country *models.Country) fyne.CanvasObject {
+	bg := canvas.NewRectangle(color.RGBA{100, 100, 100, 255})
+	flagIcon := widget.NewIcon(g.getCountryFlag(country))
+	return newFixedHeightTile(bg, container.NewCenter(flagIcon), 50)
+}
+
+func (g *Game) getCountryFlag(country *models.Country) fyne.Resource {
+	var flagResource fyne.Resource
+	var err error
+	if runtime.GOOS == "js" {
+		flagURL := fmt.Sprintf("assets/twemoji_flags_cca2/%s.svg", country.CCA2)
+		flagResource, err = fyne.LoadResourceFromURLString(flagURL)
+	} else {
+		flagPath := fmt.Sprintf("assets/twemoji_flags_cca2/%s.svg", country.CCA2)
+		flagResource, err = fyne.LoadResourceFromPath(flagPath)
+	}
+	if err != nil {
+		return nil
+	}
+	return flagResource
+}
+
+func (g *Game) addGuessRow(country *models.Country) {
+	isCorrectContinent := country.Region == g.currentCountry.Region
+
+	continentColor := color.RGBA{0, 180, 0, 255}
+	if !isCorrectContinent {
+		continentColor = color.RGBA{180, 0, 0, 255}
+	}
+
+	popIcon := g.getCompareIcon(float64(country.Population), float64(g.currentCountry.Population))
+	popColor := g.getCompareColor(float64(country.Population), float64(g.currentCountry.Population))
+
+	areaIcon := g.getCompareIcon(country.Area, g.currentCountry.Area)
+	areaColor := g.getCompareColor(country.Area, g.currentCountry.Area)
+
+	flagTile := g.createFlagTile(country)
+	countryTile := g.createTile(country.Name.Common, nil, color.RGBA{100, 100, 100, 255})
+
+	row := container.NewGridWithColumns(5,
+		flagTile,
+		countryTile,
+		g.createTile(country.Region, nil, continentColor),
+		g.createTile(fmt.Sprintf("%d", country.Population), popIcon, popColor),
+		g.createTile(fmt.Sprintf("%.0f", country.Area), areaIcon, areaColor),
+	)
+	g.bodyGrid.Add(row)
+	g.bodyGrid.Refresh()
+}
+
 func (g *Game) newGame() {
-	if len(g.countries) == 0{
+	if len(g.countries) == 0 {
 		g.statusLabel.SetText("Error loading countries data")
 		return
 	}
 
 	g.currentCountry = &g.countries[rand.Intn(len(g.countries))]
+	g.guesses = []models.Country{}
+	g.bodyGrid.RemoveAll()
+	g.guessEntry.SetText("")
+	g.guessEntry.Enable()
+	g.guessBtn.Enable()
+	g.statusLabel.SetText("Make a guess!")
 }
 
-func (g *Game) updateLabels(country *models.Country) {
-	if country == nil{
-		g.statusLabel.SetText("Enter a valid country name!")
-	}else{
-		if country.Population > g.currentCountry.Population{
-			g.popIcon.SetResource(theme.MoveDownIcon())
-		}else{
-			g.popIcon.SetResource(theme.MoveUpIcon())
-		}
-		if country.Area > g.currentCountry.Area{
-			g.areaIcon.SetResource(theme.MoveDownIcon())
-		}else{
-			g.areaIcon.SetResource(theme.MoveUpIcon())
-		}
-		g.popLabel.SetText(fmt.Sprintf("Population: %d", country.Population))
-		g.continentLabel.SetText(fmt.Sprintf("Continent: %s", country.Region))
-		g.areaLabel.SetText(fmt.Sprintf("Area: %.2f km²", country.Area)) 
-		g.popContent = container.NewHBox(g.popLabel, g.popIcon)
-		g.areaContent = container.NewHBox(g.areaLabel, g.areaIcon)
-		g.popIcon.Show()
-		g.areaIcon.Show()
-	}
-}
-
-func (g *Game) getCountry(countries []models.Country, name string) *models.Country{
-	for _, country := range countries{
-		if strings.EqualFold(country.Name.Common, name){
+func (g *Game) getCountry(countries []models.Country, name string) *models.Country {
+	for _, country := range countries {
+		if strings.EqualFold(country.Name.Common, name) {
 			return &country
 		}
 	}
@@ -129,6 +234,15 @@ func (g *Game) makeGuess() {
 		return
 	}
 
+	guessedCountry := g.getCountry(g.countries, guess)
+	if guessedCountry == nil {
+		g.statusLabel.SetText("Country not found!")
+		return
+	}
+
+	g.guesses = append(g.guesses, *guessedCountry)
+	g.addGuessRow(guessedCountry)
+
 	if strings.EqualFold(guess, g.currentCountry.Name.Common) {
 		g.statusLabel.SetText(fmt.Sprintf("Correct! It was %s!", g.currentCountry.Name.Common))
 		g.guessEntry.Disable()
@@ -137,9 +251,6 @@ func (g *Game) makeGuess() {
 	}
 
 	g.guessEntry.SetText("")
-	g.statusLabel.SetText("")
-
-	g.updateLabels(g.getCountry(g.countries, guess))
 }
 
 func (g *Game) GetContent() *fyne.Container {

5a4a5d33ef451785e047914f2720c8a65445d504
Tomáš Škarpa <tskarpam8@gmail.com>
2025-12-13 17:39:46 +0100

    feat: store the prefered selected lang in cookies

--- internal/utils/locale.go
+++ internal/utils/locale.go
@@ -10,6 +10,16 @@ import (
 )
 
 func GetSystemLocale() string {
+	// Try to get saved language from localStorage
+	localStorage := js.Global().Get("localStorage")
+	if !localStorage.IsUndefined() {
+		savedLang := localStorage.Call("getItem", "language")
+		if !savedLang.IsNull() && savedLang.String() != "" {
+			return savedLang.String()
+		}
+	}
+
+	// Fall back to browser language
 	htmlLang := js.Global().Get("document").Get("documentElement").Get("lang")
 	if !htmlLang.IsUndefined() && !htmlLang.IsNull() && htmlLang.String() != "" {
 		return strings.Split(htmlLang.String(), "-")[0]
@@ -18,6 +28,12 @@ func GetSystemLocale() string {
 }
 
 func SetSystemLocale(locale string) {
+	// Save to localStorage
+	localStorage := js.Global().Get("localStorage")
+	if !localStorage.IsUndefined() {
+		localStorage.Call("setItem", "language", locale)
+	}
+
 	js.Global().Get("document").Get("documentElement").Set("lang", locale)
 	os.Setenv("LANG", locale)
 }
